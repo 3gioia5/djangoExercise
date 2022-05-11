12ec65dafe9be732105f4490dd684b499b9b46f5
gioia <arocozy@gmail.com>
2022-05-11 22:27:16 +0900

    Learn about validation using form

--- gioia-django/costory/posts/forms.py
+++ gioia-django/costory/posts/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 from .models import Post
+from django.core.exceptions import ValidationError
 
 class PostForm(forms.ModelForm):
   
   class Meta:
     model = Post
     fields = ['title', 'content']
+
+  def clean_title(self):
+    title = self.cleaned_data['title']
+    if '*' in title:
+      raise ValidationError("'*'는 제목에 포함될 수 없습니다.")
+    return title
