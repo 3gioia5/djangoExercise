71e3e4fcb2e0994e0ec90e2849fffaf65bc75de7
gioia <arocozy@gmail.com>
2022-05-21 23:46:01 +0900

    Create user model and Set up allauth

--- django-coplate/coplate_project/coplate_project/settings.py
+++ django-coplate/coplate_project/coplate_project/settings.py
@@ -37,8 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'coplate'
+    'django.contrib.sites',
+    'coplate',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -119,3 +125,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Auth Settings
+
+AUTH_USER_MODEL = "coplate.User"
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+# Email Settings
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
