ff467e1d1c930245115eaeae12e4606c40f30a4c
gioia <arocozy@gmail.com>
2022-05-31 22:42:17 +0900

    Create ReviewDeleteView

--- django-coplate/coplate_project/coplate/urls.py
+++ django-coplate/coplate_project/coplate/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   path("reviews/<int:review_id>/", views.ReviewDetailView.as_view(), name="review-detail"),
   path("reviews/new/", views.ReviewCreateView.as_view(), name="review-create"),
   path("reviews/<int:review_id>/edit/", views.ReviewUpdateView.as_view(), name="review-update"),
+  path("reviews/<int:review_id>/delete/", views.ReviewDeleteView.as_view(), name="review-delete"),
 ]
--- django-coplate/coplate_project/coplate/views.py
+++ django-coplate/coplate_project/coplate/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from allauth.account.views import PasswordChangeView
 from django.urls import reverse
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Review
 from .forms import ReviewForm
 
@@ -41,6 +41,15 @@ class ReviewUpdateView(UpdateView):
 
   def get_success_url(self):
     return reverse("review-detail", kwargs={"review_id": self.object.id})
+  
+
+class ReviewDeleteView(DeleteView):
+  model = Review
+  template_name = "coplate/review_confirm_delete.html"
+  pk_url_kwarg = "review_id"
+
+  def get_success_url(self):
+    return reverse("index")
 
 
 class CustomPasswordChangeView(PasswordChangeView):
