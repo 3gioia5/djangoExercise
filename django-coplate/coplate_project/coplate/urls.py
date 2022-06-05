6c3caeb6854b4f7a6538308ab119c489df826221
gioia <arocozy@gmail.com>
2022-06-05 23:31:19 +0900

    Create UserReviewList page

--- django-coplate/coplate_project/coplate/urls.py
+++ django-coplate/coplate_project/coplate/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
   path("reviews/<int:review_id>/edit/", views.ReviewUpdateView.as_view(), name="review-update"),
   path("reviews/<int:review_id>/delete/", views.ReviewDeleteView.as_view(), name="review-delete"),
   path("users/<int:user_id>/", views.ProfileView.as_view(), name="profile"),
+  path("users/<int:user_id>/reviews/", views.UserReviewListView.as_view(), name="user-review-list"),
 ]
