664443e744b686d364dccf8fc25d648b239fd642
gioia <arocozy@gmail.com>
2022-06-11 01:36:49 +0900

    Create function to update profile

--- django-coplate/coplate_project/coplate_project/settings.py
+++ django-coplate/coplate_project/coplate_project/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'copalte.middleware.ProfileSetupMiddleWare',
+    'coplate.middleware.ProfileSetupMiddleWare',
 ]
 
 ROOT_URLCONF = 'coplate_project.urls'
