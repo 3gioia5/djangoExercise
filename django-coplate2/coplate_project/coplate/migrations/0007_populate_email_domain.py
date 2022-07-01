3025e9cc23d450cf6cc682872ed0822a81f4b063
gioia <arocozy@gmail.com>
2022-07-01 23:20:11 +0900

    Learn about unapplying data migration

--- django-coplate2/coplate_project/coplate/migrations/0007_populate_email_domain.py
+++ django-coplate2/coplate_project/coplate/migrations/0007_populate_email_domain.py
@@ -16,5 +16,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(save_email_domain)
+        migrations.RunPython(save_email_domain, migrations.RunPython.noop)
     ]
